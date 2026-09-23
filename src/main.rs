335703eb01397208fd8f23543a4f8a76ebfa4ce4
ItsMe <231827038+Its-Me573@users.noreply.github.com>
2026-09-23 08:07:36 -0700

    Add current directory detection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+const VALID_COMMANDS: [&str; 5] = [
+    "init",
+    "status",
+    "add",
+    "commit",
+    "log"
+];
+
+fn main() -> std::io::Result<()> {    
+    //Read the command line argument
+    let args: Vec<String> = env::args().collect();
+
+    let path = env::current_dir()?;
+    println!("The current directory is {}", path.display());
+    Ok(())
 }
