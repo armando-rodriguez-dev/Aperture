1ff2de7481f10eb859606bc17c6e33e4c076a24e
ItsMe <231827038+Its-Me573@users.noreply.github.com>
2026-09-24 20:13:42 -0700

    Add initial command validation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs; 
+
 
 const VALID_COMMANDS: [&str; 5] = [
     "init",
@@ -8,11 +10,39 @@ const VALID_COMMANDS: [&str; 5] = [
     "log"
 ];
 
+///Checks whether the command is one of the valid commads.
+/// 
+/// ### Arguments
+/// 
+/// * 'valid_command_array' - The list of valid commands.
+/// * 'command' - The command provided by the user.
+/// 
+/// ### Returns
+/// 
+/// 'true' if the command is valid, otherwise 'false'.
+fn is_valid_command(valid_command_array: [&str; 5], 
+                    command: &str
+    ) -> bool {
+
+    for valid_commands in valid_command_array {
+        if valid_commands == command {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 fn main() -> std::io::Result<()> {    
     //Read the command line argument
     let args: Vec<String> = env::args().collect();
 
+    //Store the current working directory
     let path = env::current_dir()?;
-    println!("The current directory is {}", path.display());
+
+    //Store all current directory items
+    let items_in_current_directory = fs::read_dir("./").unwrap();
+
+
     Ok(())
 }
